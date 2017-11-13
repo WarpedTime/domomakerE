19d3ae329ffddadcba39fd4401f27f3d96d1d200
WarpedTime <silverstorm79@gmail.com>
2017-11-12 22:31:53 -0500

    done

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -4,17 +4,10 @@ const Account = models.Account;
 const loginPage = (req, res) => {
   res.render('login', { csrfToken: req.csrfToken() });
 };
-
-const signupPage = (req, res) => {
-  res.render('signup', { csrfToken: req.csrfToken() });
-  res.render('signup', { csrfToken: req.csrfToken() });
-};
-
 const logout = (req, res) => {
   req.session.destroy();
   res.redirect('/');
 };
-
 const login = (request, response) => {
   const req = request;
   const res = response;
@@ -36,7 +29,6 @@ const login = (request, response) => {
     return res.json({ redirect: '/maker' });
   });
 };
-
 const signup = (request, response) => {
   const req = request;
   const res = response;
@@ -81,8 +73,19 @@ const signup = (request, response) => {
   });
 };
 
+const getToken = (request, response) => {
+  const req = request;
+  const res = response;
+  
+  const csrfJSON = {
+    csrfToken: req.csrfToken(),
+  };
+  
+  res.json(csrfJSON);
+};
+
 module.exports.loginPage = loginPage;
 module.exports.login = login;
 module.exports.logout = logout;
-module.exports.signupPage = signupPage;
 module.exports.signup = signup;
+module.exports.getToken = getToken;
